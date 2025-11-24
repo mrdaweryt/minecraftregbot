7c553e4eeb301fe9c12f3d37ea7028027c71b5d1
mrdaweryt <96956525+mrdaweryt@users.noreply.github.com>
2025-11-24 15:05:41 +0200

    Update regminebot.py
    Fix

--- regminebot.py
+++ regminebot.py
@@ -1,6 +1,6 @@
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils.executor import start_webhook
